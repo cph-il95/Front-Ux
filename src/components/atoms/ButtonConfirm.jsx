ab34fa3953cdbdb537c59f02343e0bd623dda711
Ida Ligaard <cph-il95@cphbusiness.dk>
2023-11-15 22:18:24 +0100

    confirm knap

---
+++ src/components/atoms/ButtonConfirm.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from '@mantine/core';
+
+export default function ButtonConfirm() {
+  return (
+    <Button variant="filled" color="rgba(38, 18, 18, 1)" size="xl">Confirm</Button>
+  )
+}
