d5f8f841b6cf133141cc51a4282d01ba7b8cdd7c
Emilie Midtgaard <cph-em134@cphbusiness.dk>
2023-11-17 16:11:32 +0100

    ændring i date

--- src/components/organisms/datePicker.jsx
+++ src/components/organisms/datePicker.jsx
@@ -1,22 +1,104 @@
 import React, { useState } from 'react';
 import { DatePicker } from '@mantine/dates';
+import { Grid } from '@mantine/core';
+import 'react-day-picker/lib/style.css';
+import DateTimePicker from 'react-datetime-picker';
+import 'react-datetime-picker/dist/DateTimePicker.css';
 
+
+
+
+// jeg laver komponentet DatPicker 
 const MyDatePicker = () => {
-  const [selectedDate, setSelectedDate] = useState(null);
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
+// status for at spore den valgte dato og klokkeslæt
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+
+// Funktionen til at håndtere dato og tidsændring
+  const handleDateTimeChange = (date) => {
+    setSelectedDateTime(date);
   };
 
+
+
+  
   return (
+
+    <div>
+    <h2>Select a date and time</h2>
+    <div>
     <DatePicker
-      value={selectedDate}
-      onChange={handleDateChange}
-      label="Select a date"
-      placeholder="Choose date"
-      withSelect
-    />
+      value={selectedDateTime}
+      onChange={handleDateTimeChange}
+      />
+     </div>
+
+     {selectedDateTime && (
+        <p>
+          You selected {selectedDateTime.toLocaleDateString()} at{' '}
+          {selectedDateTime.toLocaleTimeString()}
+        </p>
+      )}
+      </div>
   );
 };
 
 export default MyDatePicker;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { DatePicker } from '@mantine/dates';
+// import { Grid } from '@mantine/core';
+
+
+
+// const MyDatePicker = () => {
+//   const [selectedDate, setSelectedDate] = useState(null);
+
+//   const handleDateChange = (date) => {
+//     setSelectedDate(date);
+//   };
+
+
+
+  
+//   return (
+//     <DatePicker
+//       value={selectedDate}
+//       onChange={handleDateChange}
+//       label="Select a date"
+//       placeholder="Choose date"
+//       withSelect
+//     />
+//   );
+// };
+
+// export default MyDatePicker;
--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,10 +1,13 @@
+
 import MyDatePicker from '@/components/organisms/datePicker';
+import { Grid } from '@mantine/core';
 
 const HomePage = () => {
   return (
+
     <div>
       <h1>PICK A DAY AND TIME</h1>
-      <MyDatePicker />
+      <MyDatePicker/>
     </div>
   );
 };
