88e82e7ef915e99750742125df6401714ac51178
Emilie Midtgaard <cph-em134@cphbusiness.dk>
2023-11-17 11:43:02 +0100

    ændring i date

--- src/components/organisms/datePicker.jsx
+++ src/components/organisms/datePicker.jsx
@@ -2,29 +2,76 @@ import React from 'react'
 import { useState } from 'react';
 import { DatePicker } from '@mantine/dates';
 
-import DayPicker from 'react-day-picker';
-import 'react-day-picker/lib/style.css';
 
 
+import React, { useState } from "react";
+import useMyContext from "@/context/my-context";
+import Link from "next/link";
+import { DatePicker } from "@mantine/dates";
 
-// jeg laver dayPicker komponentet 
-const DayPickerComponent = () => {
-// jeg laver et state for at kunne spore den valgte dag
-const [selectedDay, setSelectedDay] = useState(null);
-// 
-const handleDayClick = (day) => {
-    setSelectedDay(day);
-  };
+export default function dato() {
+  const contextValue = useMyContext();
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    contextValue.setDate(event.target.value);
+  }
 
   return (
     <div>
-      <h2>Select a Day</h2>
-      <DayPicker selected={selectedDay} onDayClick={handleDayClick} />
-      {selectedDay && (
-        <p>You selected {selectedDay.toLocaleDateString()}</p>
-      )}
+      <p>Vælg dato for booking</p>
+      <div>
+        <DatePicker allowDeselect value={value} onChange={setValue} />;
+        <input type="date" value={contextValue.date} onChange={handleChange} />
+      </div>
+      <Link href="/booking/">
+        <button>to booking page</button>
+      </Link>
     </div>
   );
-};
+}
 
-export default DayPickerComponent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // jeg laver dayPicker komponentet 
+// const DayPickerComponent = () => {
+// // jeg laver et state for at kunne spore den valgte dag
+// const [selectedDay, setSelectedDay] = useState(null);
+// // 
+// const handleDayClick = (day) => {
+//     setSelectedDay(day);
+//   };
+
+//   return (
+//     <div>
+//       <h2>Select a Day</h2>
+//       <DayPicker selected={selectedDay} onDayClick={handleDayClick} />
+//       {selectedDay && (
+//         <p>You selected {selectedDay.toLocaleDateString()}</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default DayPickerComponent;
---
+++ src/pages/booking/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DayPickerComponent from '@/components/organisms/datePicker'
+
+
+const App = () => {
+    return (
+      <div>
+        <h1>My React App</h1>
+        <DayPickerComponent />
+      </div>
+    );
+  };
+  
+  export default App;
