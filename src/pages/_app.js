982d80e83936c7562276b05e5c827b5ae739cd42
Emilie Midtgaard <cph-em134@cphbusiness.dk>
2023-11-21 14:39:10 +0100

    app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,14 +6,7 @@ import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import { UserContext } from "@/context/user-context";
 
 export default function App({ Component, pageProps }) {
-  const contextValue = {
-    firstname,
-    setFirstname,
-    surname,
-    setSurname,
-    email,
-    setEmail,
-  };
+  
 
   return (
     <MantineProvider>
